d0b2f20cd41c42f4c71ac1c239a4b418292d0bea
DoubleC771 <iheinrichlouie771@gmail.com>
2025-02-26 14:17:36 +0800

    Assign letter grades using an if-elseif-else const

--- Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc60f415f490a024b952ed4508706b7221997c94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+387105173e76c52522035aca8eed033e767a941c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
